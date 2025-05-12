424a82263f9f5fdf7c91364abebc81c70e082c3b
trinidadleonard <tleonard@mail.austral.edu.ar>
2025-05-12 15:15:57 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,63 +1,54 @@
-def remove_elements(list_to_remove_elements):
-    len_lista = len(list_to_remove_elements)
-	if len_lista >= 1 and len_lista < 5:
-		del list_to_remove_elements[0]
+def remove_elements (list_to_remove_elements):
+	len_lista= len(list_to_remove_elements)
+	if len_lista>= 6 :
+		del list_to_remove_elements [5]
+		del list_to_remove_elements [4]
+		del list_to_remove_elements [0]
 		return list_to_remove_elements
-	elif len_lista == 5:
-		del list_to_remove_elements[4] 
-		del list_to_remove_elements[0]
-		return list_to_remove_elements
-	elif len_lista >= 6:
-		del list_to_remove_elements[5] 
+	elif len_lista ==5 :
 		del list_to_remove_elements[4]
-		del list_to_remove_elements[0]
+		del list_to_remove_elements [0]
 		return list_to_remove_elements
+	elif len_lista<5 and len_lista>= 1:	
+		del list_to_remove_elements [0]
+		return list_to_remove_elements	
 	else:
-		return list_to_remove_elements
-
-def add_elements(list_to_add_elements):
-    list_to_add_elements.insert(0, 'Pink')
-	list_to_add_elements.append('Yellow')
+		return []
+def add_elements (list_to_add_elements):
+	list_to_add_elements.append("Yellow")
+	list_to_add_elements.insert(0,"Pink")		
 	return list_to_add_elements
-
-def is_empty(list_to_check):
-    len_lista = len(list_to_check)
-	if len_lista == 0:
-		return "True"
+def is_empty (list_to_check):
+	if list_to_check == []:
+		return True 	
 	else:
-		return "False"
+		return False
 
-def check_lists(list_to_compare1, list_to_compare2):
-    len1 = len(list_to_compare1)
-	len2 = len(list_to_compare2)
-	if len1 >= 3 and len2 >= 3:
-		if list_to_compare1[2] == list_to_compare2[2]:
-			return "True"
+def check_lists(list_to_compare1,list_to_compare2):
+	lista1= list_to_compare1
+	lista2= list_to_compare2
+	if len (lista1)	>= 3 and len (lista2) >=3:
+		if lista1[2]==lista2[2]	:
+			valor= True
 		else:
-			return "False"
+			valor= False
 	else:
-		return "False"
+		valor=False
+	return valor
 
 def list_of_lists(list_of_lists_to_modify):
-    list0 = list_of_lists_to_modify[0]
-	list1 = list_of_lists_to_modify[1]
-	list2 = list_of_lists_to_modify[2]
-	if len(list0) > 2:
-		list0_new = [list0[0],list0[1]]
+	sub1= list_of_lists_to_modify[0]
+	sub2= list_of_lists_to_modify [1]	
+	sub3= list_of_lists_to_modify [2]	
+	if len(sub1)>0:
+		sub1=sub1[0:2]
 	else:
-		list0_new = list0
-	len1 = len(list1)
-	if len1 == 2:
-		list1_new = [list1[1]]
-	elif len1 == 3:
-		list1_new = [list1[1],list1[2]]
-	elif len1 >= 4:
-		list1_new = [list1[1],list1[2],list1[3]]
+		sub1=[]
+	if len(sub2)>= 2 :
+		sub2=sub2[1:4]
 	else:
-		list1_new = []
-	if len(list2) > 2:
-		list2_new = [list2[-2],list2[-1]]
-	else:
-		list2_new = list2
-	final_list = [list0_new, list1_new, list2_new]
-	return final_list
+		sub2=[]
+	if len (sub3) > 0:
+		sub3=sub3[-2:]	
+	todo=[sub1, sub2, sub3]
+	return todo			
